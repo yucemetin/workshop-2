8e5b49b4a95c3046f1c6b5362346355582c98ec8
yucemetin <red.banana988@gmail.com>
2023-04-06 16:09:41 +0300

    video-27

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import classes.CustomerManager;
+
 public class Main {
     public static void main(String[] args) {
-
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.add();
+        customerManager.remove();
+        customerManager.update();
     }
 }
