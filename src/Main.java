aff026bf44547af1b120c4013ef7b8ffc454e12f
yucemetin <red.banana988@gmail.com>
2023-04-06 16:16:10 +0300

    video-28-29

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         customerManager.add();
         customerManager.remove();
         customerManager.update();
+
+        int[] sayilar1 = new int[]{1, 2, 3};
+        int[] sayilar2 = new int[]{4, 5, 6};
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
     }
 }
