bff27ea9770f4a1fb79cb6ac80a932df24c4233c
yucemetin <red.banana988@gmail.com>
2023-04-06 16:21:08 +0300

    video-30

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import classes.CustomerManager;
+import recapdemo_classes.DortIslem;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +13,11 @@ public class Main {
         sayilar2 = sayilar1;
         sayilar1[0] = 10;
         System.out.println(sayilar2[0]);
+
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(24, 12));
+        System.out.println(dortIslem.cikar(24, 12));
+        System.out.println(dortIslem.bol(24, 12));
+        System.out.println(dortIslem.carp(24, 12));
     }
 }
